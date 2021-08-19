6f4c5999c731a3506c997f404430ecf393b83674
molkeyishere <89187989+molkeyishere@users.noreply.github.com>
2021-08-19 16:50:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var slideIndex = 1;
+        showSlides(slideIndex);
+        
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+        
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+        
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("dot");
+          if (n > slides.length) {slideIndex = 1}    
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
+          }
+          for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" actives", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          dots[slideIndex-1].className += " actives";
+        }
+
+        function darkMode(){
+            var element = document.body;
+            var button = document.getElementById('darkmode');
+            element.classList.toggle("dark-mode");
+            button.classList.toggle("glowbutton");
+        }
